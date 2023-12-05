20929d30d502347ff3d6f945616895b344afbf31
silver <silver@sildrid.com>
2023-12-04 23:30:53 -0300

    minor evolution data fixes

--- frontend/src/components/PokeInfo/Evolution.jsx
+++ frontend/src/components/PokeInfo/Evolution.jsx
@@ -3,6 +3,7 @@ import './Evolution.css';
 
 export default function({evolution, name}){
   const navigate = useNavigate();
+  const idMatcher = /(?<=\/)\d+(?=\/)/;
   const clearText = (txt)=>{
     return txt.replace(/[-_]/g," ");
   }
@@ -14,98 +15,112 @@ export default function({evolution, name}){
         return false;
       }
     })
-    switch(info.trigger.name){
-      case "level-up":
-        return(
-          <div>
-            <p>Lv Up</p>
-            {requirements.map(n=>{
-              switch(n){
-                case "min_level":
-                  return <p>Lv{info.min_level}+</p>
-                break;
-                case "min_happiness":
-                case "min_affection":
-                  return (
-                    <p>
-                      happy<br/>
-                      <img
-                        className="evo-item"
-                        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/soothe-bell.png"
-                      />
-                    </p>
-                  )
-                break;
-                case "time_of_day":
-                  return <p>{info.time_of_day}</p>
-                break;
-                case "location":
-                  return <p>special location</p>
-                break;
-                case "known_move":
-                  return <p>knowing {info.known_move.name}</p>
-                break;
-                case "known_move_type":
-                  return <p>knowing {info.known_move_type.name} type move</p>
-                break;
-                case "min_beauty":
-                  return <p>high beauty</p>
-                break;
-                case "relative_physical_stats":
-                  return <p>Atk{info.relative_physical_stats>0?">":"<"}Def</p>
-                break;
-                case "gender":
-                  return <p>{info.gender?"female":"male"}</p>
-                break;
-                case "held_item":
-                  return( 
-                    <p>
-                      {clearText(info.held_item.name)}<br/>
-                      <img
-                        className="evo-item"
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${info.held_item.name}.png`}
-                      />
-                    </p>
-                  )
-                break;
-                default:
-                  return <p>{clearText(n)}</p>
-                break;
-              }
-            })}
-          </div>
-        );
-      break;
-      case "use-item":
-        return(
-          <p>
-            {clearText(info.item.name)}<br/>
-            <img
-              className="evo-item"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${info.item.name}.png`}
-            />
-          </p>
-        )
-      break;
-      case "trade":
-        return(
-          <p>
-            trade<br/>
-            {info.held_item && clearText(info.held_item.name)}<br/>
-            {info.held_item && <img
-              className="evo-item"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${info.held_item.name}.png`}
-            />}
-          </p>)
-      break;
-      default:
-        return clearText(info.trigger.name);
-      break;
+    const triggerReturn = ()=>{
+      switch(info.trigger.name){
+        case "level-up":
+          return <p>Lv Up</p>
+        break;
+        default:
+          return clearText(info.trigger.name)
+        break;
+      }
     }
+    return(
+      <div>
+        {triggerReturn()}
+        {requirements.map(n=>{
+          switch(n){
+            case "gender":
+              return <p>{info.gender?"female":"male"}</p>
+            break;
+            case "held_item":
+              return( 
+                <p>
+                  {clearText(info.held_item.name)}<br/>
+                  <img
+                    className="evo-item"
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${info.held_item.name}.png`}
+                  />
+                </p>
+              )
+            break;
+            case "item":
+              return(
+                <p>
+                  {clearText(info.item.name)}<br/>
+                  <img
+                    className="evo-item"
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${info.item.name}.png`}
+                  />
+                </p>
+              )
+            break;
+            case "known_move":
+              return <p>knowing {info.known_move.name}</p>
+            break;
+            case "known_move_type":
+              return <p>knowing {info.known_move_type.name} type move</p>
+            break;
+            case "location":
+              return <p>special location</p>
+            break;
+            case "min_affection":
+            case "min_happiness":
+              return (
+                <p>
+                  happy<br/>
+                  <img
+                    className="evo-item"
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/soothe-bell.png"
+                   />
+                </p>
+              )
+            break;
+            case "min_beauty":
+              return <p>high beauty</p>
+            break;
+            case "min_level":
+              return <p>Lv{info.min_level}+</p>
+            break;
+            case "party_species":
+              return(
+                <p>
+                  <img className="evo-item" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${info.party_species.url.match(idMatcher)[0]}.png`}/>
+                  <br/>
+                  {info.party_species.name} in the party
+                </p>
+              )
+            break;
+            case "party_type":
+              return <p>{info.party_type.name} type party</p>
+            break;
+            case "relative_physical_stats":
+              return <p>Atk{info.relative_physical_stats>0?">":"<"}Def</p>
+            break;
+            case "time_of_day":
+              return <p>{info.time_of_day}</p>
+            break;
+            case "trade_species":
+              return(
+                <p>
+                  with
+                  <br/>
+                  <img className="evo-item" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${info.trade_species.url.match(idMatcher)[0]}.png`}/>
+                  <br/>
+                  {info.trade_species.name}
+                </p>
+              )
+            break;
+            default:
+              return <p>{clearText(n)}</p>
+            break;
+          }
+        })}
+      </div>
+    )
   }
 
   const evolutionTree = (entry, babyItem)=>{
-    const idMatcher = /(?<=\/)\d+(?=\/)/;
     return(
       <>
         {entry &&
